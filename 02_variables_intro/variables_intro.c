0be8b2905917117e5673b13c7bf0566268e3cc23
Katelyn McCullough <kmccullough7245@floridapoly.edu>
2025-01-16 16:33:11 +0000

    Implemented 02 Variables Intro

---
+++ 02_variables_intro/variables_intro.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() { 
+    int first_number = 100;
+    int second_number = -19;
+    printf("The first number is %d\n", first_number);
+    printf("The second number is %d\n", second_number);
+    int sum = first_number + second_number;
+    printf("%d\n+%d = %d\n", first_number + second_number, sum);
+}
---
+++ 02_variables_intro/variables_intro_practice.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    int course_id = 2271;
+    int student_id = 10234;
+    printf("The course id is: %d\n", course_id);
+    printf("The student id is: %d\n", student_id);
+    int sum = student_id - course_id;
+    printf("The difference between the student id and the course id is: %d\n", sum);
+}

a2d51a48d05a8e6f235213f70b55db8541d64008
Katelyn McCullough <kmccullough7245@floridapoly.edu>
2025-01-16 16:30:49 +0000

    Implemented 01 First Program

---
+++ 01_first_program/first_program.c
@@ -0,0 +1,12 @@
+// one-line comment
+// This is the first program which prints a welcome message.
+/*
+Multi line comment
+*/
+// Create a main function.
+#include <stdio.h>
+int main( void ) {
+    printf("Welcome to COP2271!");
+    printf("I want to code.");
+    return 0;
+}
